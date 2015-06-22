33943bf7bfca2c782eb879361a71cbdca455480b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-22 18:29:12 +0200

    Test and implement

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
+import dummyData from './module/index';
+
 const test = require('tape-catch');
+
+test('Works as it should', (is) => {
+  const docs = ['a', {b: 2}, 3, true, null, undefined];
+
+  is.deepEqual(
+    dummyData(docs),
+    {docs, version: 1},
+    'no kidding!'
+  );
+
+  is.end();
+});
